b06bfe5d19169c9b624ea7f6e987a6294aaf5971
“sandraldr27” <sandralilder@gmail.com>
2023-06-24 19:59:59 +0200

    Environments folder is generated, app image files are added, a background image and scss styles are inserted

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  //apiUrl: 'https://api.example.com',
+  //apiKey: 'YOUR_API_KEY',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  //apiUrl: 'https://api.example.com',
+  //apiKey: 'YOUR_API_KEY',
+};
